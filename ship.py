8b773bfbe7d485d6e9e7859f97b33c6be16fd442
Josh Grant <Josh_grant1@baylor.edu>
2021-10-23 00:10:43 -0500

    limited ships range

--- ship.py
+++ ship.py
@@ -18,13 +18,20 @@ class Ship():
          # Movement flag
         self.moving_right = False
         self.moving_left = False
+    
+    
     def update(self):
         #Update the ship's position based on the movement flag. 
-        if self.moving_right:
+        # Update the ship's center value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.center += self.ai_settings.ship_speed_factor
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.center -= self.ai_settings.ship_speed_factor
 
+        
+        
+        
+        
         # Update rect object from self.center.
         self.rect.centerx = self.center
 
