b104d0f326800a6d5e722afc89535095f88a8b04
Josh Grant <Josh_grant1@baylor.edu>
2021-10-22 23:24:31 -0500

    created game_functions and modified alien_invasion

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     # Initialize game, settings and create a screen object.
@@ -19,9 +20,7 @@ def run_game():
     # Start the main loop for the game.
     while True:
         # Watch for keyboard and mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events()
         
         
         # Redraw the screen during each pass through the loop.
