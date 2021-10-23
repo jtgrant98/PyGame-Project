bd679dc6f9c1ec9d21eb717f8a00fe6dcde839d6
Josh Grant <Josh_grant1@baylor.edu>
2021-10-23 00:32:38 -0500

    deleting, limiting, and updating bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,7 +25,17 @@ def run_game():
         # Watch for keyboard and mouse events.
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        bullets.update()
+        gf.update_bullets(bullets)
+
+
+        # Get rid of bullets that have disappeared.
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                 bullets.remove(bullet)
+        print(len(bullets))
+        
+        
+        
         gf.update_screen(ai_settings, screen, ship, bullets)
 
         
--- game_functions.py
+++ game_functions.py
@@ -10,8 +10,9 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
     # Create a new bullet and add it to the bullets group.
-        new_bullet = Bullet(ai_settings, screen, ship) 
-        bullets.add(new_bullet)
+        if len(bullets) < ai_settings.bullets_allowed:
+            new_bullet = Bullet(ai_settings, screen, ship)
+            bullets.add(new_bullet)
 
 
 
@@ -51,5 +52,15 @@ def update_screen(ai_settings, screen, ship, bullets):
     ship.blitme()
     # Make the most recently drawn screen visible.
     pygame.display.flip()
+
+
+def update_bullets(bullets):
+    """Update position of bullets and get rid of old bullets."""
+    # Update bullet positions.
+    bullets.update()
+    # Get rid of bullets that have disappeared.
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
 
 
