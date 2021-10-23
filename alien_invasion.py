e18d03eff81ecd8558591cbc53f52c2dd9d9d4bf
Josh Grant <Josh_grant1@baylor.edu>
2021-10-23 00:03:23 -0500

    adjusted the ship's speed

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,7 +12,7 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     # Make a ship.
-    ship = Ship(screen)
+    ship = Ship(ai_settings,screen)
 
     # Set the background color.
     bg_color = (230, 230, 230)
--- ship.py
+++ ship.py
@@ -1,8 +1,9 @@
 import pygame
 class Ship():
-    def __init__(self, screen):
+    def __init__(self, ai_settings,screen):
         """Initialize the ship and set its starting position."""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         # Load the ship image and get its rect.
         self.image = pygame.image.load('/Users/joshgrant/Downloads/ship.bmp')
@@ -11,15 +12,21 @@ class Ship():
         # Start each new ship at the bottom center of the screen.
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+         
+        # Store a decimal value for the ship's center.
+        self.center = float(self.rect.centerx)
          # Movement flag
         self.moving_right = False
         self.moving_left = False
     def update(self):
         #Update the ship's position based on the movement flag. 
         if self.moving_right:
-            self.rect.centerx += 1
+            self.center += self.ai_settings.ship_speed_factor
         if self.moving_left:
-            self.rect.centerx -= 1
+            self.center -= self.ai_settings.ship_speed_factor
+
+        # Update rect object from self.center.
+        self.rect.centerx = self.center
 
     
     
