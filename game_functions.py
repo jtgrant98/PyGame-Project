f0124c710e60e0800dcfb1d412d793d44017614e
Josh Grant <Josh_grant1@baylor.edu>
2021-10-23 22:19:21 -0500

    updating score as aliens are shot

--- game_functions.py
+++ game_functions.py
@@ -194,8 +194,10 @@ def update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_bu
     pygame.display.flip()
 
 
-def update_bullets(ai_settings, screen, ship, aliens, bullets):
+def update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets):
     """Update position of bullets and get rid of old bullets."""
+    check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship,
+        aliens, bullets)
     # Update bullet positions.
     bullets.update()
     # Get rid of bullets that have disappeared.
@@ -206,12 +208,15 @@ def update_bullets(ai_settings, screen, ship, aliens, bullets):
     check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
 
 
-def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
+def check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship, aliens, bullets):
     #"""Respond to bullet-alien collisions."""
     # Remove any bullets and aliens that have collided.
     # Check for any bullets that have hit aliens.
     # If so, get rid of the bullet and the alien.
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+    if collisions:
+        stats.score += ai_settings.alien_points
+        sb.prep_score()
 
     if len(aliens) == 0:
     # Destroy existing bullets, speed up game, and create new fleet.
